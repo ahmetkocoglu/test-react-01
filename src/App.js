fce32889a813260cdb57f31bd2714e3dc9694d0b
ahmet <ahmetkocoglu@outlook.com>
2022-09-30 12:50:49 +0300

    test proje

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {storage} from "./store/user";
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
+import * as api from "./service";
+
 function App() {
+  const count = useSelector(state => state.user.value)
+  const dispatch = useDispatch()
+
+  const [email, setEmail] = useState("admin@system.com");
+  const [password, setPassword] = useState("123123");
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+
+  const onClickSave = async () => {
+    const response = await api.post('auth/login', {email, password});
+    console.log(response);
+    setToken(response.data.data.token);
+    dispatch(storage(response.data.data.token));
+  }
+
+  const onClickGet = async () => {
+    const response = await api.get('/user/appData');
+    console.log(response.data.user.firstname);
+    setUser(response.data.user);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={e => setEmail(e.target.value)} value={email} />
+      <input type={password} onChange={e => setPassword(e.target.value)} value={password} />
+      <button onClick={onClickSave}>Login</button>
+      {token}
+      <button onClick={onClickGet}>Get</button>
+      <hr/>
+      {`${user.firstname} ${user.lastname} ${user.fullname} ${user.email}`}
+      <hr/>
+      {count}
     </div>
   );
 }
